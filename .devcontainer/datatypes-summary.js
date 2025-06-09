fc5530e97d71c715dae6c257a4fb6c854b979585
Sandip Kumar <157000310+75498783@users.noreply.github.com>
2025-06-09 19:05:45 +0000

    datatypes-summry

---
+++ .devcontainer/datatypes-summary.js
@@ -0,0 +1,29 @@
+// primitive their are have 7 type 
+//string , number, boolean , null, undefine, bigint
+
+ const score  = 100
+ const scorevalue = 100.3
+
+ const isLoggedIn = false
+ const outsideTemp = null 
+  let userEmail;
+
+  const id = Symbol('123')
+  const anotherId = Symbol('123')
+  
+  console.log(id === anotherId);
+  
+
+//Referencetype(Non primitive)
+
+// Array , objects , Functions
+const heros = ["sandip", " mandip ", " Akash"," Alok",];
+let myobj = {
+    name : "sunny",
+    age : 22,
+    mobilenum: 7549878364
+}
+const myaFunction  = function(){
+    console.log("hello world");
+    
+}
