b70219738a7ae730fd637afe78ffbd3a9a45fb3c
Sandip Kumar <157000310+75498783@users.noreply.github.com>
2025-06-09 18:29:21 +0000

    Comparision

---
+++ .devcontainer/Comparision.js
@@ -0,0 +1,13 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// === stric is check value and value datatype..
+
+console.log("2"===2);
